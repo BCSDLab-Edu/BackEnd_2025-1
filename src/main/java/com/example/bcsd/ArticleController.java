fa337830013e96300ad3136c6c77f1ed330b90a4
Walk1277 <jisu1445@gmail.com>
2025-05-04 02:56:10 +0900

    GET 메서드 구현

--- src/main/java/com/example/bcsd/ArticleController.java
+++ src/main/java/com/example/bcsd/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.bcsd;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,4 +11,15 @@ import java.util.List;
 public class ArticleController {
     List<Article> articleList = new ArrayList<>();
     int currentId = 1;
+
+    // GET /articles/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticle(@PathVariable int id) {
+        for (Article article : articleList) {
+            if (article.getId() == id) {
+                return ResponseEntity.ok(article);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
