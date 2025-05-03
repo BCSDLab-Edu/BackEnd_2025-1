bf5ffc331208b6caf0f3a5f7e37d0d0109126d93
Walk1277 <jisu1445@gmail.com>
2025-05-04 02:55:07 +0900

    데이터를 저장할 리스트, id를 설정할 변수 선언

--- src/main/java/com/example/bcsd/ArticleController.java
+++ src/main/java/com/example/bcsd/ArticleController.java
@@ -2,6 +2,12 @@ package com.example.bcsd;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/articles")
-public class ArticleController { }
+public class ArticleController {
+    List<Article> articleList = new ArrayList<>();
+    int currentId = 1;
+}
