181f42e869f97c5c9fb20cc6646fe2e4e854f21e
Walk1277 <jisu1445@gmail.com>
2025-05-04 00:09:47 +0900

    introduce?name=이름 으로 요청했을때 hello 이름! 텍스트가 반환되도록 하기

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -3,6 +3,7 @@ package com.example.bcsd;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -18,8 +19,24 @@ public class HelloController {
         return "hello";
     }
 
+    @ResponseBody
     @GetMapping("/introduce")
-    public String introduce() {
-        return "introduce";
+    public Object introduce(@RequestParam(required = false) String name) {
+        if (name !=null) { return "hello " + name + "!"; }
+        else  {
+            return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>자기소개</title>
+                </head>
+                <body>
+                <p>안녕하세요. 저는 임지수입니다.</p>
+                <p>나이는 23, 컴퓨터공학부 2학년이고, BCSD Backend Beginner입니다.</p>
+                </body>
+                </html>
+                """;
+        }
     }
 }
