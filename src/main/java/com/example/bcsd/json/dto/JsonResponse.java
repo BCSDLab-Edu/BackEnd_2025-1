bbadbe10fad5795bbddcf4506dc45ddc06f4ef7f
Caaasperr <9183612@gmail.com>
2025-05-04 12:42:53 +0900

    feat: json 요청 구현

---
+++ src/main/java/com/example/bcsd/json/dto/JsonResponse.java
@@ -0,0 +1,11 @@
+package com.example.bcsd.json.dto;
+
+public class JsonResponse {
+    public String name;
+    public int age;
+
+    public JsonResponse(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
