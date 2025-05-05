5091953c02036a3e42cc66fde11d01516f7c05da
mgm136044 <mkm0530@koreatech.ac.kr>
2025-05-05 19:17:13 +0900

    refactor: /introduce 요청받았을 때 이름 출력 구현+/introduce?name={이름}을 입력 받았을 때 {이름} 반환

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -3,18 +3,16 @@ package com.example.bcsd;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
     @ResponseBody
     @GetMapping("/introduce")
-    public String hello() {
-        return "안녕하세요 제 이름은 민경민입니다!";
-    }
-
-    @GetMapping("/introduce?")
-    public String hello2() {
-        return "hello";
+    public String introduce(@RequestParam(required = false) String name) {
+        if (name == null||name.isEmpty()) {
+            return "안녕하세요 제 이름은 민경민입니다!";
+        }
+        return "안녕하세요 제 이름은 "+name+"입니다!";
     }
 }
