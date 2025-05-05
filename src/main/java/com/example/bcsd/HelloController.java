cd7ee55ec63facfebb110b66640dbad40141941a
mgm136044 <mkm0530@koreatech.ac.kr>
2025-05-05 21:27:36 +0900

    feat: json요청 했을 때 반환기능 구현 refactor:/introduce 요청 했을 때 HTML 반환하도록 변경

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @ResponseBody
+
     @GetMapping("/introduce")
     public String introduce(@RequestParam(required = false) String name) {
         if (name == null||name.isEmpty()) {
-            return "안녕하세요 제 이름은 민경민입니다!";
+            return "MyName";
         }
         return "안녕하세요 제 이름은 "+name+"입니다!";
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public UserInfo getUser(){
+        return new UserInfo(23,"민경민");
     }
 }
