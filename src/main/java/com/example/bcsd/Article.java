d02398012009df8fd6c3e8be064c813f7aa495fd
Walk1277 <jisu1445@gmail.com>
2025-05-12 01:21:41 +0900

    Article 클래스 author, date 필드 추가

--- src/main/java/com/example/bcsd/Article.java
+++ src/main/java/com/example/bcsd/Article.java
@@ -1,13 +1,19 @@
 package com.example.bcsd;
 
+import java.time.LocalDateTime;
+
 public class Article {
     int id;
     String title;
+    String author;
+    LocalDateTime date;
     String content;
 
-    public Article(int id, String title, String content) {
+    public Article(int id, String title, String author, LocalDateTime date, String content) {
         this.id = id;
         this.title = title;
+        this.author = author;
+        this.date = date;
         this.content = content;
     }
 
@@ -16,6 +22,12 @@ public class Article {
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public LocalDateTime getDate() { return date; }
+    public void setDate(LocalDateTime date) { this.date = date; }
 
     public String getContent() { return content; }
     public void setContent(String content) { this.content = content; }
