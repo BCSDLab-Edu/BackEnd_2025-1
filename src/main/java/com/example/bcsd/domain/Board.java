c8b7cfc944a3933aa469748c296f98b3d9bdbb78
LeeYejin013 <remitjoy0413@gmail.com>
2025-05-24 22:20:06 +0900

    refactor: getter 추가

--- src/main/java/com/example/bcsd/domain/Board.java
+++ src/main/java/com/example/bcsd/domain/Board.java
@@ -3,4 +3,17 @@ package com.example.bcsd.domain;
 public class Board {
     private Long id;
     private String name;
+
+    public Board(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
